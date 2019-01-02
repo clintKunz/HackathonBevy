e4fab89bcf97fa4a796e57858bdcc1ae9e34d8a1
wisdomtooth <cbtsao47@gmail.com>
2019-01-02 14:47:11 -0600

    add styles to Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
 
-import Navigation from './components/Navigation';
-
+//components
+import Navigation from "./components/Navigation";
+import LogIn from "./components/LogIn.js";
+library.add(faEnvelope, faKey);
 const Container = styled.div`
-  max-width: 500px; 
-  margin: 0 auto; 
-  height: 100vh; 
-  border: 1px solid red; 
+  max-width: 500px;
+  margin: 0 auto;
+  height: 100vh;
+  border: 1px solid red;
 `;
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Container>
         <Navigation />
+        <Route exact path="/login" render={props => <LogIn {...props} />} />
       </Container>
     );
   }

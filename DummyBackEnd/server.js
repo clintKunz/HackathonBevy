ef10d26ca11a84d57304768c639c31e1c1e5eb1e
Drewsup123c <drewsup123c@gmail.com>
2019-01-02 17:29:27 -0700

    added cors to server

--- DummyBackEnd/server.js
+++ DummyBackEnd/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const db = require('./database/dbConfig.js');
+const cors = require('cors');
 const server = express();
 server.use(express.json());
+server.use(cors())
 
 
 server.get('/', (req,res) => {

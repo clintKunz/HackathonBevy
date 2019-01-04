00d92711a0976cf0a916357bbc49e8c29fb09a63
Clint Kunz <clintkunz@gmail.com>
2019-01-04 11:58:31 -0700

    solicitpage fix

--- front-end/src/components/SolicitPage.js
+++ front-end/src/components/SolicitPage.js
@@ -32,7 +32,7 @@ class SolicitPage extends React.Component {
   componentDidMount(){
     axios.get(`http://localhost:5000/api/loans/${this.props.match.params.id}`)
     .then(res => {
-      this.setState({solicit : res.data})
+      this.setState({solicit : res.data.loan[0]})
       console.log(this.state.solicit)
     })
     // console.log(this.props.match.params.id)
@@ -45,8 +45,8 @@ class SolicitPage extends React.Component {
     return (
       <SolicitContainer>
           <h2>{(this.state.solicit.solicitType === 'borrower') ? 'In search of a qualified lender to agree to or counter the following terms:' : 'In search of a qualified borrower to agree to the following terms:'} </h2>
-          <p>I, <span>"username"</span>, hereby solicit a <span>{this.state.solicit.loanType}</span> loan in the amount of <span>${this.state.solicit.amount}</span>. With the following terms,</p>
-          <p>The loan will be funded by the lender no later than two weeks after both parties have signed the contract. Payback for the loan will then commence on the day the contract is executed or <span>{moment(this.state.solicit.startDate).format('MMM-DD-YYYY')}, whichever is later.</span> Payments will be made monthly for the entire length of the loan, which is <span>{this.state.solicit.lengthMonths}</span> months. Monthly payments will be $<span>{Math.round((this.state.solicit.amount/this.state.solicit.lengthMonths)*(1 + this.state.solicit.interest/100/12))}</span></p>
+          <p>I hereby solicit a <span>{this.state.solicit.loanType}</span> loan in the amount of <span>${this.state.solicit.amount}</span>. With the following terms,</p>
+          <p>The loan will be funded by the lender no later than two weeks after both parties have signed the contract. Payback for the loan will then commence on the day the contract is executed or <span>{moment(this.state.solicit.startDate).format('MMM-DD-YYYY')}</span>, whichever is later. Payments will be made monthly for the entire length of the loan, which is <span>{this.state.solicit.lengthMonths}</span> months. Monthly payments will be <span>${Math.round((this.state.solicit.amount/this.state.solicit.lengthMonths)*(1 + this.state.solicit.interest/100/12))}</span></p>
           <h3>Reason why I need the loan:</h3>
           <p><span>{this.state.solicit.pitch}.</span></p>
           <p>If you are interested in fulfilling this loan, contact me at <span>example@hotmail.com</span>. Thank you!</p>        

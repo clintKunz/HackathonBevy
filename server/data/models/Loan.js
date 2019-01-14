9caf59a61f819c7a2c9e10fcfc28a0706fc1c74a
William Pelton <williamrpelton@gmail.com>
2019-01-14 01:45:39 -0600

    Update Loan.js

--- server/data/models/Loan.js
+++ server/data/models/Loan.js
@@ -6,13 +6,12 @@ const LoanSchema = new mongoose.Schema(LoanContract);
 
 LoanSchema.pre('save', async function() {
     let { isSigned, solicitType, solicitedBy } = this;
-    if (solicitType === "borrower") this.borrower = solicitedBy;
-    else if (solicitType === "lender") this.lender = solicitedBy;
+    this[solicitType] = solicitedBy;
     if (isSigned && !signedOn) this.signedOn = Date.now();
     const borrowerDoc = await User.findById(this.borrower);
     const lenderDoc = await User.findById(this.lender);
     if (borrowerDoc) await borrowerDoc.update({ $addToSet: { loans: this._id } });
     if (lenderDoc) await lenderDoc.update({ $addToSet: { loans: this._id } });
-})
+});
 
 module.exports = mongoose.model('Loan', LoanSchema);
